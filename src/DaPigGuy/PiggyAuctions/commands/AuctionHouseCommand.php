e7c800559c78f528c5567f011f0dc5dbe82a5404
DaPigGuy <mcpepig123@gmail.com>
2019-10-23 22:15:43 -0700

    Sort by ending soon + Show time remaining

--- src/DaPigGuy/PiggyAuctions/commands/AuctionHouseCommand.php
+++ src/DaPigGuy/PiggyAuctions/commands/AuctionHouseCommand.php
@@ -18,6 +18,7 @@ use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\item\Item;
 use pocketmine\nbt\tag\IntTag;
 use pocketmine\Player;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
 
 /**
@@ -60,6 +61,23 @@ class AuctionHouseCommand extends BaseCommand
         $page = $args["page"] ?? 1;
         $pageAuctions = $this->displayPageAuctions($menu->getInventory(), $page);
 
+        $updateTask = new ClosureTask(function (int $currentTick) use ($menu, $page) : void {
+            foreach ($menu->getInventory()->getContents() as $slot => $content) {
+                if ($content->getNamedTagEntry("AuctionID") !== null) {
+                    $auction = $this->plugin->getAuctionManager()->getAuction($content->getNamedTagEntry("AuctionID")->getValue());
+                    if ($auction === null || $auction->hasExpired()) {
+                        $this->displayPageAuctions($menu->getInventory(), $page);
+                        continue;
+                    }
+                    $lore = $content->getLore();
+                    $lore[count($lore) - 1] = "Ends in " . $this->formatDuration($auction->getEndDate() - time());
+                    $content->setLore($lore);
+                    $menu->getInventory()->setItem($slot, $content);
+                }
+            }
+        });
+        $this->plugin->getScheduler()->scheduleRepeatingTask($updateTask, 1);
+
         $menu->setListener(function (Player $player, Item $itemClicked, Item $itemClickedWith, SlotChangeAction $action) use ($page, $pageAuctions): bool {
             if (isset($pageAuctions[$action->getSlot()])) {
                 $auction = $pageAuctions[$action->getSlot()];
@@ -70,6 +88,9 @@ class AuctionHouseCommand extends BaseCommand
                 $page = $itemClicked->getNamedTagEntry("Page")->getValue();
             }
             return false;
+        });
+        $menu->setInventoryCloseListener(function () use ($updateTask): void {
+            if ($updateTask->getHandler() !== null) $updateTask->getHandler()->cancel();
         });
         $menu->send($sender);
     }
@@ -84,6 +105,9 @@ class AuctionHouseCommand extends BaseCommand
         $inventory->clearAll(false);
 
         $activeAuctions = $this->plugin->getAuctionManager()->getActiveAuctions();
+        uasort($activeAuctions, function (Auction $a, Auction $b): bool {
+            return $a->getEndDate() > $b->getEndDate();
+        }); //TODO: Changeable sort type
         /** @var Auction $auction */
         foreach (array_slice($activeAuctions, ($page - 1) * 45, 45) as $slot => $auction) {
             $item = clone $auction->getItem();
@@ -92,11 +116,19 @@ class AuctionHouseCommand extends BaseCommand
                 "",
                 self::TF_RESET . "Seller: " . $auction->getAuctioneer(),
                 self::TF_RESET . "Bids: " . TextFormat::GREEN . count($auction->getBids()),
+                ""
             ]);
-            if ($auction->getTopBid() !== null) $lore = array_merge($lore, [
+            if ($auction->getTopBid() !== null) {
+                $lore = array_merge($lore, [
+                    self::TF_RESET . "Top Bid: " . TextFormat::GOLD . $auction->getTopBid()->getBidAmount(),
+                    self::TF_RESET . "Bidder: " . TextFormat::GOLD . $auction->getTopBid()->getBidder(),
+                ]);
+            } else {
+                $lore[] = self::TF_RESET . "Starting Bid: " . TextFormat::GOLD . 100; //TODO: Add start bid cuz im stupid and forgot
+            }
+            $lore = array_merge($lore, [
                 "",
-                self::TF_RESET . "Top Bid: " . TextFormat::GOLD . $auction->getTopBid()->getBidAmount(),
-                self::TF_RESET . "Bidder: " . TextFormat::GOLD . $auction->getTopBid()->getBidder()
+                "Ends in " . $this->formatDuration($auction->getEndDate() - time())
             ]);
 
             $item->setNamedTagEntry(new IntTag("AuctionID", $auction->getId()));
@@ -113,6 +145,30 @@ class AuctionHouseCommand extends BaseCommand
             $inventory->setItem(53, $nextPage);
         }
         return array_slice($activeAuctions, ($page - 1) * 45, 45);
+    }
+
+    /**
+     * @param int $duration
+     * @return string
+     */
+    public function formatDuration(int $duration): string
+    {
+        $days = floor($duration / 86400);
+        $hours = floor($duration / 3600 % 24);
+        $minutes = floor($duration / 60 % 60);
+        $seconds = floor($duration % 60);
+
+        if ($days >= 1) {
+            $dateString = $days . "d";
+        } elseif ($hours > 6) {
+            $dateString = $hours . "h";
+        } elseif ($minutes >= 1) {
+            $dateString = ($hours > 0 ? $hours . "h" : "") . $minutes . "m" . ($seconds == 0 ? "" : $seconds . "s");
+        } else {
+            $dateString = $seconds . "s";
+        }
+
+        return $dateString;
     }
 
     /**
